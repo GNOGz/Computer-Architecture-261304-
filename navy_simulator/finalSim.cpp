9307fb037a107ceee4f19357d292dfea01dba7cd
navyney <141761309+navyney@users.noreply.github.com>
2025-10-19 00:05:00 +0700

    edit function prototype (i write it wrong sorry guys)

--- navy_simulator/finalSim.cpp
+++ navy_simulator/finalSim.cpp
@@ -23,12 +23,13 @@ struct Instruction {
     int destOrOffset;
 };
 
+int signExtend16(int num);
 void printState(stateType *);
 void fprintState(ofstream &out, stateType *statePtr);
-int signExtend16(int num);
 void loadProgram(const string &filename, stateType &state, ofstream &outFile);
 void initializeMachine(stateType &state);
 void executeProgram(stateType &cpu, ofstream &outFile);
+Instruction decodeInstruction(int machineCode);
 
 int main(int argc, char *argv[]) {
     if (argc < 2 || argc > 3) {
@@ -68,6 +69,7 @@ void printState(stateType *statePtr) {
     cout << "end state\n";
 }
 
+//print the state to an output file (for easier debugging )
 void fprintState(ofstream &out, stateType *statePtr) {
     out << "\n@@@\nstate:\n";
     out << "\tpc " << statePtr->pc << "\n";
@@ -99,6 +101,7 @@ Instruction decodeInstruction(int machineCode) {
     return inst;
 }
 
+//read in the input file (machine-code file) into memory and also print it out to terminal and output file
 void loadProgram(const string &filename, stateType &state, ofstream &outFile) {
     ifstream file(filename);
     if (!file.is_open()) {
